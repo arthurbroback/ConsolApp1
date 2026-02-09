63b9e7ebc84af359959761b4701fc99453e531b3
arthurbroback <arthur.broback@gmail.com>
2026-02-09 15:03:17 +0100

    första commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+using (var doc = WordprocessingDocument.Create("example.excel", WordprocessingDocumentType.Document))
+{
+    var mainPart = doc.AddMainDocumentPart();
+    mainPart.Document =
+        new Document(new Body(new Paragraph(new Run(new Text("Hello, Open XML!")))));
+}
+
+Console.WriteLine("Word-filen skapades!");
